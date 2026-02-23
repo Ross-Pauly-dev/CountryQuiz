f3f33e69a9bfb8b451969dc9fbc55aa4ef601588
Ross-Pauly-dev <rossapauly@gmail.com>
2026-02-23 14:56:07 -0500

    Using base url of the github page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/CountryQuiz">
       <Routes>
         <Route path="/quiz" element={<QuizInProgress />} />
         <Route
