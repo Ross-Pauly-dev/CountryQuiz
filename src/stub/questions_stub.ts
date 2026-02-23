3683f67f8940faeac4d91e2774c906b88558d55d
Ross-Pauly-dev <rossapauly@gmail.com>
2026-02-23 11:14:45 -0500

    Added stub data for local dev

---
+++ src/stub/questions_stub.ts
@@ -0,0 +1,117 @@
+export const questions = [
+    {
+        id: '1',
+        question: 'What is the capital of France?',
+        correctAnswerId: '1',
+        answers: [
+            {
+                id: '1',
+                text: 'Paris',
+            },
+            {
+                id: '2',
+                text: 'London',
+            },
+            {
+                id: '3',
+                text: 'Berlin',
+            },
+            {
+                id: '4',
+                text: 'Madrid',
+            },
+        ],
+    },
+    {
+        id: '2',
+        question: 'what is the currency symbol of Mauritius?',
+        correctAnswerId: '2',
+        answers: [
+            {
+                id: '1',
+                text: 'USD',
+            },
+            {
+                id: '2',
+                text: 'Rs',
+            },
+            {
+                id: '3',
+                text: 'EUR',
+            },
+            {
+                id: '4',
+                text: 'GBP',
+            },
+        ],
+    },
+    {
+        id: '3',
+        question: 'Guinea-Bissau is a country in which continent?',
+        correctAnswerId: '4',
+        answers: [
+            {
+                id: '1',
+                text: 'South America',
+            },
+            {
+                id: '2',
+                text: 'Asia',
+            },
+            {
+                id: '3',
+                text: 'Europe',
+            },
+            {
+                id: '4',
+                text: 'Africa',
+            },
+        ],
+    },
+    {
+        id: '4',
+        question: 'What is the population of Russia?',
+        correctAnswerId: '1',
+        answers: [
+            {
+                id: '1',
+                text: '146028325',
+            },
+            {
+                id: '2',
+                text: '132228325',
+            },
+            {
+                id: '3',
+                text: '216028325',
+            },
+            {
+                id: '4',
+                text: '132228325',
+            },
+        ],
+    },
+    {
+        id: '5',
+        question: 'The flag 🇬🇧 belongs to which country?',
+        correctAnswerId: '2',
+        answers: [
+            {
+                id: '1',
+                text: 'France',
+            },
+            {
+                id: '2',
+                text: 'United Kingdom',
+            },
+            {
+                id: '3',
+                text: 'United States',
+            },
+            {
+                id: '4',
+                text: 'Canada',
+            },
+        ],
+    }
+]
